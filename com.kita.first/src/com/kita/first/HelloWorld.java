34010ce7d04faa767c17dc1b966807ce9cc948e9
Juyeong728 <j8025y@naver.com>
2020-12-29 15:07:12 +0900

    20.12.22 ~ 20.12.29 JAVA1 백업

---
+++ com.kita.first/src/com/kita/first/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.kita.first;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		System.out.print("Hello World!\n"); // \: escape 문자
+		System.out.print("Hello World!");
+	}
+
+}
