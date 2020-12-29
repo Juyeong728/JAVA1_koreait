f061c690225173139dd414b99c700a01ab373564
Juyeong728 <j8025y@naver.com>
2020-12-29 15:51:08 +0900

    20.12.29 JAVA1 If문

--- com.kita.first/src/com/kita/first/ConditionOperator.java
+++ com.kita.first/src/com/kita/first/ConditionOperator.java
@@ -4,6 +4,7 @@ public class ConditionOperator {
 	public static void main(String[] args) {
 		// 조건연산자(삼항연산자) A ? B : C
 		int n1 = 3;
+		int n2 = 2;
 		boolean isOdd = (n1 % 2 == 1) ? true : false;
 		System.out.println(isOdd);
 		
