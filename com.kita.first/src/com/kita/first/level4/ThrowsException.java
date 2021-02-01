d743ade87dc16aff2f4cb3b50e1bce64875b333c
Juyeong728 <j8025y@naver.com>
2021-02-01 15:19:54 +0900

    21.02.01 try catch문

--- com.kita.first/src/com/kita/first/level4/ThrowsException.java
+++ com.kita.first/src/com/kita/first/level4/ThrowsException.java
@@ -6,16 +6,37 @@ public class ThrowsException {
 	Scanner scan = new Scanner(System.in);
 	
 	void method() throws Exception {
-//		int num = Integer.parseInt(scan.next());
+		
 	}
 	
 	void method1() {
 		try {
 			
+		} catch(NumberFormatException e) {
+			
 		} catch(NullPointerException e) { //catch로 예외를 여러 개 걸 수 있음
 			
 		} catch(Exception e) { //모든 예외를 잡기 위해 제일 마지막에 걸어줌
 			
+		} finally {
+			
+		}
+	}
+	
+	void parseInputStrToInt() throws Exception {
+		System.out.print("숫자를 입력해주세요: ");
+		String str = scan.next();
+		int num = 0;
+		try {
+			num = Integer.parseInt(str);
+			System.out.println(num);
+		} catch(NumberFormatException e) {
+			System.out.println("문자가 섞였습니다. 다시 입력해주세요.");
+		} catch(NullPointerException e) {
+			System.out.println("주소값이 연결되어 있지 않습니다.");
+		} catch(Exception e) {
+			System.out.println("잘못 입력하셨습니다.");
+			num = 0;
 		} finally {
 			
 		}
