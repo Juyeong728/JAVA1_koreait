b7aed5b5829daa1607a31c5c616d7b6edcd401c0
Juyeong728 <j8025y@naver.com>
2021-02-01 15:46:53 +0900

    21.02.01

--- com.kita.first/src/com/kita/first/level4/Test.java
+++ com.kita.first/src/com/kita/first/level4/Test.java
@@ -2,12 +2,30 @@ package com.kita.first.level4;
 
 public class Test {
 	public static void main(String[] args) {
-		ThrowsException te = new ThrowsException();
+		Parent p = new Parent() {
+			@Override
+			void parentMethod() {
+				System.out.println("자식객체입니다.");
+			};
+		};
+		p.parentMethod();
+		
+		Child child = new Child();
+		child.childMethod2(new Parent() {
+			@Override
+			void parentMethod() {};
+		});
+		
+		
+		
+		ThrowsException2 te2 = new ThrowsException2();
 		try {
-			te.method();
+			te2.method1();
 		} catch(Exception e) {
 			
 		}
+		
+		
 		
 		AkmuAlbum album1 = new MusicPlayer(); //AkmuAlbum을 계승받은 MusicPlayer는 들어감
 		IUAlbum album2 = new MusicPlayer();
